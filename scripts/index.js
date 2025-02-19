71ddea41199910bb65caa2da0839c89070ef6eb7
Morozov M <m.a.m2003@mail.ru>
2025-02-19 16:38:59 +0300

    первая сдача

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,33 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const placeList = document.querySelector('.places__list')
+
+
+
+const deleteCard = (cardElement) => {
+    cardElement.remove()
+}
+
+const addCard = initialCards => {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+    cardElement.querySelector('.card__image').src = initialCards.link
+    cardElement.querySelector('.card__image').alt = initialCards.name
+    cardElement.querySelector('.card__title').textContent = initialCards.name
+
+    placeList.append(cardElement)
+
+    const deleteButton = cardElement.querySelector('.card__delete-button')
+
+    deleteButton.addEventListener('click', () => deleteCard(cardElement))
+}
+
+
+
+
+
+
+initialCards.forEach(addCard)
